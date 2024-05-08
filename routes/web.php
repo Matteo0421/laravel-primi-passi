d1106d36f21c87dccc5ab92581aff7d454e125e4
Matteo0421 <matteo.carola99@gmail.com>
2024-05-08 14:23:41 +0200

    [add]first-milestone

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route ::get('/', function(){
+    return view('home', ['name' => 'ugo']);
+})->name('home');
+
+Route ::get('/about', function(){
+    return view('about');
+})->name('about');
